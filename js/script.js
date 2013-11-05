d80702210e48c66e78081f1317bcb73c8a928a06
tan <tannm@smartosc.com>
2013-11-05 17:05:17 +0700

    first step

--- js/script.js
+++ js/script.js
@@ -1,31 +1,23 @@
 $(document).ready(function() {
-	//scrollorama
-    var scrollorama = $.scrollorama({
-        blocks:'.scrollblock'
-    });
-    scrollorama.animate('#block1',{
-        duration:400, property:'opacity'
-    });
-
     //scroll to
-    $('#toHeader').click(function(){
-    	$.scrollTo( $('header'), 800 );
-    });
-    $('#toBlock1').click(function(){
-    	$.scrollTo( $('#block1'), 800 );
-    });
-    $('#toFooter').click(function(){
-    	$.scrollTo( $('footer'), 800 );
-    });
+    // $('#toHeader').click(function(){
+    // 	$.scrollTo( $('header'), 800 );
+    // });
+    // $('#toBlock1').click(function(){
+    // 	$.scrollTo( $('#block1'), 800 );
+    // });
+    // $('#toFooter').click(function(){
+    // 	$.scrollTo( $('footer'), 800 );
+    // });
 
     //calculate height
     var winHeight = $(window).height();
-    $('.block').css('min-height', winHeight);
+    $('.story').css('min-height', winHeight);
 
     //snapWindow
     $('.scrollblock .block').windows({
         snapping: true,
-        snapSpeed: 500,
+        snapSpeed: 800,
         snapInterval: 1100,
         onScroll: function(scrollPos){
             // scrollPos:Number
@@ -37,4 +29,53 @@ $(document).ready(function() {
             // when new window ($el) enters viewport
         }
     });
+
+    //localScroll
+    $('.scrollblock').localScroll(800);
+
+    $('#header').parallax("50%", 0.1);
+    $('#block1').parallax("50%", 0.1);
+    $('.bg').parallax("50%", 0.4);
+    $('#footer').parallax("50%", 0.3);
+
+
+    //parallax
+    var $window = $(window);
+    var $header = $('#header');
+    var $block1 = $('#block1');
+    var $footer = $('#footer');
+    var pika = $("#block1 .bg");
+
+    //inview
+    $('#header, #block1, #footer').bind('inview', function (event, visible) {
+        if (visible == true){
+            $(this).addClass("inview");
+        }
+        else{
+          	$(this).removeClass("inview");
+        }
+    });
+
+    function newPos(x, winHeight, pos, adjuster, inertia){
+    	return x + "% " + (-((winHeight + pos) - adjuster) * inertia)  + "px";
+    }
+    function Move(){
+    	var pos = $window.scrollTop();
+    	if($header.hasClass("inview")){
+    	    $header.css({'backgroundPosition': newPos(50, windowHeight, pos, 900, 0.3)});
+    	}
+    	if($block1.hasClass("inview")){
+    	    $block1.css({'backgroundPosition': newPos(50, windowHeight, pos, 900, 0.3)});
+    	    pika.css({'backgroundPosition': newPos(50, windowHeight, pos, 900, 0.3)});
+    	}
+    	if($footer.hasClass("inview")){
+    	    $footer.css({'backgroundPosition': newPos(50, windowHeight, pos, 900, 0.3)});
+    	}
+	}
+});
+
+$(window).resize(function(){
+	//calculate height
+    var winHeight = $(window).height();
+    $('.story').css('min-height', winHeight);
 });
